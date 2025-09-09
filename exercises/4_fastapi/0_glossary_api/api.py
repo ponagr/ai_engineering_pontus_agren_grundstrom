ae47a5077c3385acd9cad004a03a39682f914611
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2025-09-09 11:22:33 +0200

    first task done, creating and testing glossary api with fastapi

---
+++ exercises/4_fastapi/0_glossary_api/api.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, Query
+from data_processing import glossary_data, Glossary
+from pprint import pprint
+from pydantic import ValidationError
+
+
+
+try:
+    glossarys = glossary_data("fastapi_glossary.json")
+    # glossary = [{"word":glos.word, "meaning":glos.meaning} for glos in glossary_list]
+    pprint(glossarys)
+except ValidationError as err:
+    pprint(err)
+
+
+app = FastAPI()
+
+# a) Now create an endpoint `/glossary` which will return all words and their meaning.
+@app.get("/glossary")
+async def read_glossary():
+    return [glos for glos in glossarys]
+
+
+# b) Create a query parameter to filter out a specific word
+@app.get("/glossary/")
+async def filter_glossary(word: str = Query(None, description="Filter by word in glossary")):
+    if word:
+        return [glos for glos in glossarys if glos.word.casefold() == word.casefold()]
+
+
+# c) Turn your API into a CRUD API, so that you can add glossary, update and delete glossary.
+@app.post("/glossary/add_glossary")
+async def add_glossary(glossary_request: Glossary):
+    new_glossary = Glossary.model_validate(glossary_request)
+    glossarys.append(new_glossary)
+    
+    return new_glossary
+
+
+@app.put("/glossary/update_glossary")
+async def update_glossary(updated_glossary: Glossary):
+    for i, glos in enumerate(glossarys):
+        if glos.id == updated_glossary.id:
+            glossarys[i] = updated_glossary
+    
+    return updated_glossary
+
+
+@app.delete("/glossary/delete_glossary/{id}")
+async def delete_glossary(id: int):
+    for i, glos in enumerate(glossarys):
+        if id == glos.id:
+            deleted = glossarys[i]
+            del glossarys[i]
+            return f"Deleted: {deleted}", f"Updated glossary: {glossarys}"
+            # break
--- exercises/4_fastapi/0_glossary_api/data_processing.py
+++ exercises/4_fastapi/0_glossary_api/data_processing.py
@@ -1,12 +1,40 @@
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, Field, ValidationError
 import json
+from constants import DATA_PATH
+from pprint import pprint
 
-def read_json(file_path):
-with open("fastapi_glossary.json", "r") as f:
-    data = f.read()
-data = json.loads(data)
+
+# Read the data from this repository called `fastapi_glossary.json`. Create Pydantic model(s) of the data in a separate script called `data_processing.py`.
+
+def read_json(file_name):
+    with open(DATA_PATH/file_name, "r") as file:
+        return json.load(file)
+        
 
 class Glossary(BaseModel):
-    id: int
+    id: int = Field(gt=-1)
     word: str 
     meaning: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 15,
+                "word": "serialization",
+                "meaning": "Converting a python-object(dict, list) into a json-object(string)."
+            }
+        }
+    }
+
+
+def glossary_data(filename):
+    json_data = read_json(filename)
+    return [Glossary.model_validate(item) for item in json_data]
+
+
+if __name__ == "__main__":
+    try:
+        glossary = glossary_data("fastapi_glossary.json")
+        pprint(glossary)
+    except ValidationError as err:
+        pprint(err)
